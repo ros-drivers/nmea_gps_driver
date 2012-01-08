1d66cd7afcdc8f2ce7d587ee1a8b8c9e5de74bc9
Eric Perko <wisesage5001@gmail.com>
2012-01-08 00:11:28 -0500

    Refactored the nmea_gps_driver a bunch and added support for the GGA sentence

--- src/nmea_gps_driver.py
+++ src/nmea_gps_driver.py
@@ -4,22 +4,25 @@ import roslib
 roslib.load_manifest('nmea_gps_driver')
 import rospy
 from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatStatus
 from geometry_msgs.msg import TwistStamped
 #from cyphy_ros_gps.msg import GPStime
 
 import serial, string, math
-import decimal as dec
 
 if __name__ == "__main__":
     #init publisher
-    gpspub = rospy.Publisher('GPS', NavSatFix)
-    gpsVelPub = rospy.Publisher('GPSVel',TwistStamped)
+    gpspub = rospy.Publisher('fix', NavSatFix)
+    gpsVelPub = rospy.Publisher('vel',TwistStamped)
 #    gpstimePub = rospy.Publisher('GPStime', GPStime)
     rospy.init_node('nmea_gps_driver')
     #Init GPS port
     GPSport = rospy.get_param('~port','/dev/ttyUSB0')
     GPSrate = rospy.get_param('~rate',4800)
     frame_id = rospy.get_param('~frame_id','gps')
+    useRMC = rospy.get_param('~useRMC', False)
+    #useRMC == True -> generate info from RMC+GSA
+    #useRMC == False -> generate info from GGA
     navData = NavSatFix()
     gpsVel = TwistStamped()
     #gpstime = GPStime()
@@ -37,49 +40,92 @@ if __name__ == "__main__":
             fields = data.split(',')
             for i in fields:
                 i = i.strip(',')
-            #Check for satellite lock
-            if fields[0].find('GSA') > 0:
-                lockState = int(fields[2])
-                #print 'lockState=',lockState
-                if lockState == 3:
-                    GPSLock = True
+            if useRMC:
+                #Check for satellite lock
+                if 'GSA' in fields[0]:
+                    lockState = int(fields[2])
+                    #print 'lockState=',lockState
+                    if lockState == 3:
+                        GPSLock = True
+                    else:
+                        GPSLock = False
+                #if not satellite lock message parse it separately
                 else:
-                    GPSLock = False
-            #if not satellite lock message parse it separately
+                    if GPSLock == True:
+                        if 'RMC' in fields[0]:
+                            #print fields
+                            gpsVel.header.stamp = timeNow
+                            gpsVel.twist.linear.x = float(fields[7])*0.514444444444*math.sin(float(fields[8]))
+                            gpsVel.twist.linear.y = float(fields[7])*0.514444444444*math.cos(float(fields[8]))
+                            gpsVelPub.publish(gpsVel)
+
+                            navData.status.status = NavSatStatus.STATUS_FIX
+                            navData.header.stamp = gpsVel.header.stamp
+                            navData.status.service = NavSatStatus.SERVICE_GPS
+
+                            #gpstime.header.stamp = gpsVel.header.stamp
+                            #gpstime.gpstime = rospy.Time(float(fields[1]))
+
+                            longitude = float(fields[5][0:3]) + float(fields[5][3:])/60
+                            if fields[6] == 'W':
+                                longitude = -longitude
+
+                            latitude = float(fields[3][0:2]) + float(fields[3][2:])/60
+                            if fields[4] == 'S':
+                                latitude = -latitude
+
+                            #publish data
+                            navData.latitude = latitude
+                            navData.longitude = longitude
+                            navData.altitude = float('NaN')
+                            navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_UNKNOWN
+                            gpspub.publish(navData)
+                            #gpstimePub.publish(gpstime)
+                    else:
+                        pass
+                        #print data
             else:
-                if GPSLock == True:
-                    if fields[0].find('RMC') > 0:
-                        #print fields
-                        gpsVel.header.stamp = timeNow
-                        gpsVel.twist.linear.x = float(fields[7])*0.514444444444*math.sin(float(fields[8]))
-                        gpsVel.twist.linear.y = float(fields[7])*0.514444444444*math.cos(float(fields[8]))
-                        gpsVelPub.publish(gpsVel)
-                    
-                        navData.status.status = GPSLock-1
-                        navData.header.stamp = gpsVel.header.stamp
-                        navData.status.service = 1
-                        
-                        #gpstime.header.stamp = gpsVel.header.stamp
-                        #gpstime.gpstime = rospy.Time(float(fields[1]))
+                #Use GGA
+                #No /vel output from just GGA
+                if 'GGA' in fields[0]:
+                    gps_quality = int(fields[6])
+                    if gps_quality == 0:
+                        navData.status.status = NavSatStatus.STATUS_NO_FIX
+                    elif gps_quality == 1:
+                        navData.status.status = NavSatStatus.STATUS_FIX
+                    elif gps_quality == 2:
+                        navData.status.status = NavSatStatus.STATUS_SBAS_FIX
+                    elif gps_quality in (4,5):
+                        #Maybe 2 should also sometimes be GBAS... but pretty
+                        #sure RTK has to have a base station
+                        navData.status.status = NavSatStatus.STATUS_GBAS_FIX
+                    else:
+                        navData.status.status = NavSatStatus.STATUS_NO_FIX
+                    navData.status.service = NavSatStatus.SERVICE_GPS
 
-                        longitude = dec.Decimal('%.10f' % (dec.Decimal(fields[5][0:3]) + dec.Decimal(fields[5][3:fields[5].__len__()]) / 60))
-                        if fields[6] == 'W':
-                            longitude = -longitude
+                    navData.header.stamp = timeNow
 
-                        latitude = dec.Decimal('%.10f' % (dec.Decimal(fields[3][0:2]) + dec.Decimal(fields[3][2:fields[3].__len__()]) / 60))
-                        if fields[4] == 'S':
-                            latitude = -latitude
+                    latitude = float(fields[2][0:2]) + float(fields[2][2:])/60
+                    if fields[3] == 'S':
+                        latitude = -latitude
+                    navData.latitude = latitude
 
-                        #publish data
-                        navData.latitude = latitude
-                        navData.longitude = longitude
-                        navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_UNKNOWN
-                        gpspub.publish(navData)
-                        #gpstimePub.publish(gpstime)
-          
-                else:
-                    pass
-                    #print data
+                    longitude = float(fields[4][0:3]) + float(fields[4][3:])/60
+                    if fields[5] == 'W':
+                        longitude = -longitude
+                    navData.longitude = longitude
+
+                    hdop = float(fields[8])
+                    navData.position_covariance[0] = hdop**2
+                    navData.position_covariance[4] = hdop**2
+                    navData.position_covariance[8] = (2*hdop)**2 #FIX ME
+                    navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+
+                    #Altitude is above ellipsoid, so adjust for mean-sea-level
+                    altitude = float(fields[9]) + float(fields[11])
+                    navData.altitude = altitude
+
+                    gpspub.publish(navData)
 
     except rospy.ROSInterruptException:
         GPS.close() #Close GPS serial port
