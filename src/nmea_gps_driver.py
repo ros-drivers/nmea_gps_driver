e60f2cf4d156b3583b20b51f08f6002b14a36ef0
Eric Perko <wisesage5001@gmail.com>
2012-02-20 22:37:02 -0500

    Updated to include TimeReference output

--- src/nmea_gps_driver.py
+++ src/nmea_gps_driver.py
@@ -5,30 +5,46 @@ roslib.load_manifest('nmea_gps_driver')
 import rospy
 from sensor_msgs.msg import NavSatFix
 from sensor_msgs.msg import NavSatStatus
+from sensor_msgs.msg import TimeReference
 from geometry_msgs.msg import TwistStamped
 #from cyphy_ros_gps.msg import GPStime
 
-import serial, string, math
+import serial, string, math, time, calendar
+
+#nmea_utc should be a string of form hhmmss
+def convertNMEATimeToROS(nmea_utc):
+    #Get current time in UTC for date information
+    utc_struct = time.gmtime() #immutable, so cannot modify this one
+    utc_list = list(utc_struct)
+    hours = int(nmea_utc[0:2])
+    minutes = int(nmea_utc[2:4])
+    seconds = int(nmea_utc[4:6])
+    utc_list[3] = hours
+    utc_list[4] = minutes
+    utc_list[5] = seconds
+    unix_time = calendar.timegm(tuple(utc_list))
+    return rospy.Time.from_sec(unix_time)
 
 if __name__ == "__main__":
     #init publisher
+    rospy.init_node('nmea_gps_driver')
     gpspub = rospy.Publisher('fix', NavSatFix)
     gpsVelPub = rospy.Publisher('vel',TwistStamped)
-#    gpstimePub = rospy.Publisher('GPStime', GPStime)
-    rospy.init_node('nmea_gps_driver')
+    gpstimePub = rospy.Publisher('time_reference', TimeReference)
     #Init GPS port
     GPSport = rospy.get_param('~port','/dev/ttyUSB0')
-    GPSrate = rospy.get_param('~rate',4800)
+    GPSrate = rospy.get_param('~baud',4800)
     frame_id = rospy.get_param('~frame_id','gps')
+    time_ref_source = rospy.get_param('~time_ref_source', frame_id)
     useRMC = rospy.get_param('~useRMC', False)
     #useRMC == True -> generate info from RMC+GSA
     #useRMC == False -> generate info from GGA
     navData = NavSatFix()
     gpsVel = TwistStamped()
-    #gpstime = GPStime()
+    gpstime = TimeReference()
+    gpstime.source = frame_id
     navData.header.frame_id = frame_id
     gpsVel.header.frame_id = frame_id
-    #gpstime.header.frame_id = frame_id
     GPSLock = False
     try:
         GPS = serial.Serial(port=GPSport, baudrate=GPSrate, timeout=2)
@@ -63,8 +79,8 @@ if __name__ == "__main__":
                             navData.header.stamp = gpsVel.header.stamp
                             navData.status.service = NavSatStatus.SERVICE_GPS
 
-                            #gpstime.header.stamp = gpsVel.header.stamp
-                            #gpstime.gpstime = rospy.Time(float(fields[1]))
+                            gpstime.header.stamp = gpsVel.header.stamp
+                            gpstime.time_ref = convertNMEATimeToROS(fields[1])
 
                             longitude = float(fields[5][0:3]) + float(fields[5][3:])/60
                             if fields[6] == 'W':
@@ -80,7 +96,7 @@ if __name__ == "__main__":
                             navData.altitude = float('NaN')
                             navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_UNKNOWN
                             gpspub.publish(navData)
-                            #gpstimePub.publish(gpstime)
+                            gpstimePub.publish(gpstime)
                     else:
                         pass
                         #print data
@@ -126,6 +142,10 @@ if __name__ == "__main__":
                     navData.altitude = altitude
 
                     gpspub.publish(navData)
+
+                    gpstime.header.stamp = timeNow
+                    gpstime.time_ref = convertNMEATimeToROS(fields[1])
+                    gpstimePub.publish(gpstime)
 
     except rospy.ROSInterruptException:
         GPS.close() #Close GPS serial port
