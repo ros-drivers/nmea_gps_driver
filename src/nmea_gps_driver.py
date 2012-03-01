0e3163df0de8a514f0e8445d213c339e313d5263
Eric Perko <wisesage5001@gmail.com>
2012-02-29 23:56:53 -0500

    Added catch statement for the ValueError thrown when the NMEA sentence's fields are empty and we try to convert to float. Fixes Bug #2.

--- src/nmea_gps_driver.py
+++ src/nmea_gps_driver.py
@@ -119,96 +119,100 @@ if __name__ == "__main__":
             fields = data.split(',')
             for i in fields:
                 i = i.strip(',')
-            if useRMC:
-                #Check for satellite lock
-                if 'GSA' in fields[0]:
-                    lockState = int(fields[2])
-                    #print 'lockState=',lockState
-                    if lockState == 3:
-                        GPSLock = True
+            try:
+                if useRMC:
+                    #Check for satellite lock
+                    if 'GSA' in fields[0]:
+                        lockState = int(fields[2])
+                        #print 'lockState=',lockState
+                        if lockState == 3:
+                            GPSLock = True
+                        else:
+                            GPSLock = False
+                    #if not satellite lock message parse it separately
                     else:
-                        GPSLock = False
-                #if not satellite lock message parse it separately
+                        if GPSLock == True:
+                            if 'RMC' in fields[0]:
+                                #print fields
+                                gpsVel.header.stamp = timeNow
+                                gpsVel.twist.linear.x = float(fields[7])*0.514444444444*math.sin(float(fields[8]))
+                                gpsVel.twist.linear.y = float(fields[7])*0.514444444444*math.cos(float(fields[8]))
+                                gpsVelPub.publish(gpsVel)
+
+                                navData.status.status = NavSatStatus.STATUS_FIX
+                                navData.header.stamp = gpsVel.header.stamp
+                                navData.status.service = NavSatStatus.SERVICE_GPS
+
+                                gpstime.header.stamp = gpsVel.header.stamp
+                                gpstime.time_ref = convertNMEATimeToROS(fields[1])
+
+                                longitude = float(fields[5][0:3]) + float(fields[5][3:])/60
+                                if fields[6] == 'W':
+                                    longitude = -longitude
+
+                                latitude = float(fields[3][0:2]) + float(fields[3][2:])/60
+                                if fields[4] == 'S':
+                                    latitude = -latitude
+
+                                #publish data
+                                navData.latitude = latitude
+                                navData.longitude = longitude
+                                navData.altitude = float('NaN')
+                                navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_UNKNOWN
+                                gpspub.publish(navData)
+                                gpstimePub.publish(gpstime)
+                        else:
+                            pass
+                            #print data
                 else:
-                    if GPSLock == True:
-                        if 'RMC' in fields[0]:
-                            #print fields
-                            gpsVel.header.stamp = timeNow
-                            gpsVel.twist.linear.x = float(fields[7])*0.514444444444*math.sin(float(fields[8]))
-                            gpsVel.twist.linear.y = float(fields[7])*0.514444444444*math.cos(float(fields[8]))
-                            gpsVelPub.publish(gpsVel)
-
+                    #Use GGA
+                    #No /vel output from just GGA
+                    if 'GGA' in fields[0]:
+                        gps_quality = int(fields[6])
+                        if gps_quality == 0:
+                            navData.status.status = NavSatStatus.STATUS_NO_FIX
+                        elif gps_quality == 1:
                             navData.status.status = NavSatStatus.STATUS_FIX
-                            navData.header.stamp = gpsVel.header.stamp
-                            navData.status.service = NavSatStatus.SERVICE_GPS
+                        elif gps_quality == 2:
+                            navData.status.status = NavSatStatus.STATUS_SBAS_FIX
+                        elif gps_quality in (4,5):
+                            #Maybe 2 should also sometimes be GBAS... but pretty
+                            #sure RTK has to have a base station
+                            navData.status.status = NavSatStatus.STATUS_GBAS_FIX
+                        else:
+                            navData.status.status = NavSatStatus.STATUS_NO_FIX
+                        navData.status.service = NavSatStatus.SERVICE_GPS
 
-                            gpstime.header.stamp = gpsVel.header.stamp
-                            gpstime.time_ref = convertNMEATimeToROS(fields[1])
+                        navData.header.stamp = timeNow
 
-                            longitude = float(fields[5][0:3]) + float(fields[5][3:])/60
-                            if fields[6] == 'W':
-                                longitude = -longitude
+                        latitude = float(fields[2][0:2]) + float(fields[2][2:])/60
+                        if fields[3] == 'S':
+                            latitude = -latitude
+                        navData.latitude = latitude
 
-                            latitude = float(fields[3][0:2]) + float(fields[3][2:])/60
-                            if fields[4] == 'S':
-                                latitude = -latitude
+                        longitude = float(fields[4][0:3]) + float(fields[4][3:])/60
+                        if fields[5] == 'W':
+                            longitude = -longitude
+                        navData.longitude = longitude
 
-                            #publish data
-                            navData.latitude = latitude
-                            navData.longitude = longitude
-                            navData.altitude = float('NaN')
-                            navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_UNKNOWN
-                            gpspub.publish(navData)
-                            gpstimePub.publish(gpstime)
-                    else:
-                        pass
-                        #print data
-            else:
-                #Use GGA
-                #No /vel output from just GGA
-                if 'GGA' in fields[0]:
-                    gps_quality = int(fields[6])
-                    if gps_quality == 0:
-                        navData.status.status = NavSatStatus.STATUS_NO_FIX
-                    elif gps_quality == 1:
-                        navData.status.status = NavSatStatus.STATUS_FIX
-                    elif gps_quality == 2:
-                        navData.status.status = NavSatStatus.STATUS_SBAS_FIX
-                    elif gps_quality in (4,5):
-                        #Maybe 2 should also sometimes be GBAS... but pretty
-                        #sure RTK has to have a base station
-                        navData.status.status = NavSatStatus.STATUS_GBAS_FIX
-                    else:
-                        navData.status.status = NavSatStatus.STATUS_NO_FIX
-                    navData.status.service = NavSatStatus.SERVICE_GPS
+                        hdop = float(fields[8])
+                        navData.position_covariance[0] = hdop**2
+                        navData.position_covariance[4] = hdop**2
+                        navData.position_covariance[8] = (2*hdop)**2 #FIX ME
+                        navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
 
-                    navData.header.stamp = timeNow
+                        #Altitude is above ellipsoid, so adjust for mean-sea-level
+                        altitude = float(fields[9]) + float(fields[11])
+                        navData.altitude = altitude
 
-                    latitude = float(fields[2][0:2]) + float(fields[2][2:])/60
-                    if fields[3] == 'S':
-                        latitude = -latitude
-                    navData.latitude = latitude
 
-                    longitude = float(fields[4][0:3]) + float(fields[4][3:])/60
-                    if fields[5] == 'W':
-                        longitude = -longitude
-                    navData.longitude = longitude
+                        gpstime.header.stamp = timeNow
+                        gpstime.time_ref = convertNMEATimeToROS(fields[1])
 
-                    hdop = float(fields[8])
-                    navData.position_covariance[0] = hdop**2
-                    navData.position_covariance[4] = hdop**2
-                    navData.position_covariance[8] = (2*hdop)**2 #FIX ME
-                    navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
-
-                    #Altitude is above ellipsoid, so adjust for mean-sea-level
-                    altitude = float(fields[9]) + float(fields[11])
-                    navData.altitude = altitude
-
-                    gpspub.publish(navData)
-
-                    gpstime.header.stamp = timeNow
-                    gpstime.time_ref = convertNMEATimeToROS(fields[1])
-                    gpstimePub.publish(gpstime)
+                        gpspub.publish(navData)
+                        gpstimePub.publish(gpstime)
+            except ValueError as e:
+                rospy.logwarn("Value error, likely due to missing fields in the NMEA messages. Error was: %s" % e)
 
     except rospy.ROSInterruptException:
         GPS.close() #Close GPS serial port
