7e9760d3eeb51e2bb1c16a3d47492e9667c50539
Eric Perko <wisesage5001@gmail.com>
2012-05-12 16:48:00 -0400

    Checksum checking was failing if the sentence did not have the checksum * character
    This fixes KForge ticket #4
    https://kforge.ros.org/gpsdrivers/trac/ticket/4

--- src/nmea_gps_driver.py
+++ src/nmea_gps_driver.py
@@ -70,8 +70,11 @@ def addTFPrefix(frame_id):
 #Check the NMEA sentence checksum. Return True if passes and False if failed
 def check_checksum(nmea_sentence):
     split_sentence = nmea_sentence.split('*')
+    if len(split_sentence) != 2:
+        #No checksum bytes were found... improperly formatted/incomplete NMEA data?
+        return False
     transmitted_checksum = split_sentence[1].strip()
-    
+
     #Remove the $ at the front
     data_to_checksum = split_sentence[0][1:]
     checksum = 0
