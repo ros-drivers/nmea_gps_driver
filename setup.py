1dbc41a59f188b6ddc8d7a0d6f3f4573587972d4
Eric Perko <wisesage5001@gmail.com>
2013-02-10 19:46:32 -0500

    Renamed old serial driver back to groovy name for tick-tock. Installing it to proper location instead of global bin directory.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
         packages=[],
-        scripts=['scripts/nmea_gps_driver'],
         package_dir={ '' : 'src' }
     )
 
